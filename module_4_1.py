004dca5f17bc1af64e55ddea2abb0d8c9dc8f418
olgalvova2024 <olga.lvova1317@mail.ru>
2024-07-13 21:44:02 +0300

    Задача "А как делить?"

---
+++ module_4_1.py
@@ -0,0 +1,6 @@
+from fake_math import divide as divide_zero_non
+from true_math import divide as divide_zero_inf
+print(divide_zero_non(159,13))
+print(divide_zero_non(753,0))
+print(divide_zero_inf(456,321))
+print(divide_zero_inf(1,0))
